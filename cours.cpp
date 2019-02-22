f160605dd191a5387d04d0497c2178dc859189af
Stag LDNR <stagldnr@debian-java-ldnr.ldnr.local>
2019-02-22 14:00:15 +0100

    test2

---
+++ cours.cpp
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+//creation fichier demo 2 la ou on est 
+touch demo2.md
+// ecrire HOP la dans mon fichier demo2
+echo "HOP LA" > demo2.md
+// modifier le fichier depuis le SHELL
+vi demo2.md
+//Sauvegarder depuis le SHELL le fichier ouvert
+:wq
+
